56d3357014648e4c7e2677758f3bf2eb1993bc15
Shariar <shariarnafis86@gmail.com>
2023-12-11 17:36:13 +0600

    nav bar done

---
+++ src/Menu.jsx
@@ -0,0 +1,46 @@
+/* eslint-disable react/prop-types */
+
+import { Link } from 'react-scroll';
+import { AnimatePresence, motion } from "framer-motion"
+import { useState } from 'react';
+
+
+const Menu = ({item}) => {
+    const [isHover, setIsHover] = useState(false)
+
+    return (
+        
+            <a href={item?.uri} key={item?.id} className="text-gray-300 p-1 rounded-full hover:text-gray-800 cursor-pointer hover:bg-[#00FF7D] relative" 
+          onMouseEnter={()=>setIsHover(true)}
+          onMouseLeave={()=>setIsHover(false)}
+          >
+            <Link 
+            
+            activeClass="active"
+            to={item?.id}
+            spy={true}
+            smooth={true}
+            offset={-50}
+            duration={500}
+
+          >
+            {item?.Icon}
+            <AnimatePresence>
+              {
+                isHover && (<motion.div
+                initial={{opacity: 0, x: -25}}
+                animate={{opacity: 1, x: 0}}
+                exit={{opacity: 0, x: -25}}
+                   className="absolute bg-white rounded-md px-3 py-1 -right-10 -top-14 text-gray-800 font-semibold lg:-top-0 lg:right-16 after:absolute after:w-3 after:h-3 after:bg-white lg:after:top-[10px] 
+              after:-bottom-1 after:right-5 lg:after:-right-[3px] after:rotate-45">
+                  {item?.name}
+                </motion.div>)
+              }
+            </AnimatePresence>
+          </Link>
+          </a>
+        
+    );
+};
+
+export default Menu;
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,66 +1,67 @@
 import {
-    FaDiagramProject,
-    FaEnvelope,
-    FaFacebookF,
-    FaGithub,
-    FaHouse,
-    FaLinkedinIn,
-    FaRenren,
-    FaUser,
-    FaYoutube,
-  } from "react-icons/fa6";
-  import { Link } from 'react-scroll';
+  FaDiagramProject,
+  FaEnvelope,
+  FaFacebookF,
+  FaGithub,
+  FaHouse,
+  FaLinkedinIn,
+  FaRenren,
+  FaUser,
+  FaYoutube,
+} from "react-icons/fa6";
+import { useState } from "react";
+import Menu from "./Menu";
+
 
 
 const Navbar = () => {
 
-     const menus = [
-        {
-          id: 'home',
-          Icon: <FaHouse></FaHouse>,
-          name: "Home",
-        },
-        {
-          id: "about",
-          Icon: <FaUser></FaUser>,
-          name: "About",
-        },
-        {
-          id: "skills",
-          Icon: <FaRenren></FaRenren>,
-          name: "Skills",
-        },
-        {
-          id: "projects",
-          Icon: <FaDiagramProject></FaDiagramProject>,
-          name: "Projects",
-        },
-        {
-          id: "contact",
-          Icon: <FaEnvelope></FaEnvelope>,
-          name: "Contact",
-        },
-      ];
 
-    return (
-        <div className="fixed bottom-0 right-0 lg:top-0 lg:mr-3 w-full h-32 lg:h-screen lg:w32 flex justify-center lg:justify-end items-end lg:items-center pb-8 lg:pb-0 z-50 ">
-            <div className="px-4  lg:py-12 rounded-full border border-[rgba(255,255,255,0.3)] flex flex-row lg:flex-col gap-12 items-center justify-center duration-200 backdrop-blur-md">
-            {
-                menus?.map(item=><Link key={item?.id} 
-                    activeClass="active" 
-                    to={item?.id} 
-                    spy={true} 
-                    smooth={true} 
-                    offset={-50} 
-                    duration={500} 
-                  >
-                    {item?.Icon}
-                  </Link>)
-            }
-            </div>
-            
-        </div>
-    );
+  const menus = [
+    {
+      id: 'home',
+      uri: '#home',
+      Icon: <FaHouse></FaHouse>,
+      name: "Home",
+    },
+    {
+      id: "about",
+      uri: "#about",
+      Icon: <FaUser></FaUser>,
+      name: "About",
+    },
+    {
+      id: "skills",
+      uri: "#skills",
+      Icon: <FaRenren></FaRenren>,
+      name: "Skills",
+    },
+    {
+      id: "projects",
+      uri: "#projects",
+      Icon: <FaDiagramProject></FaDiagramProject>,
+      name: "Projects",
+    },
+    {
+      id: "contact",
+      uri: "#contact",
+      Icon: <FaEnvelope></FaEnvelope>,
+      name: "Contact",
+    },
+  ];
+
+  return (
+    <div className="fixed bottom-0 right-0 lg:top-0 lg:mr-3 w-full h-32 lg:h-screen lg:w32 flex justify-center lg:justify-end items-end lg:items-center pb-8 lg:pb-0 z-50 ">
+      <div className="px-4 py-3 lg:py-12 rounded-full border border-[rgba(255,255,255,0.3)] flex flex-row lg:flex-col gap-12 items-center justify-center duration-200 backdrop-blur-md">
+        {
+          menus?.map((item) => 
+          <Menu key={item.id} item={item}></Menu>
+          )
+        }
+      </div>
+
+    </div>
+  );
 };
 
 export default Navbar;
